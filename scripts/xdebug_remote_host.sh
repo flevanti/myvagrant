41db220fef6847555cebac9a167d5fec56c42cc7
Francesco Levanti <francesco@levanti.net>
2015-07-25 21:27:11 +0100

    Be sure to include xdebug ini file in the last position to override any previous value

--- scripts/xdebug_remote_host.sh
+++ scripts/xdebug_remote_host.sh
@@ -1,17 +1,18 @@
-SETTINGS_FILE="/etc/php5/cli/conf.d/99-xdebug_remote_host.ini"
+SETTINGS_FILE="/etc/php5/cli/conf.d/zzzzzzzzzzzzzzzzzz-xdebug_remote_host.ini"
 echo "-----------------------------------------------"
 echo "This script can be used to update XDEBUG remote host IP"
 echo "Configuration file: $SETTINGS_FILE"
 echo "RUN THIS SCRIPT WITH ROOT PRIVILEGES"
 echo "-----------------------------------------------"
-echo "Please enter your HOST machine IP: "
+echo "Please enter your HOST machine IP : "
 read HOST_IP
 
-# DELETE THE CONTENT OF THE FILE
-echo "" > $SETTINGS_FILE
-# APPEND CONFIGURATION PARAMETERS
-echo "xdebug.remote_host = $HOST_IP" >>  $SETTINGS_FILE
-echo "xdebug.remote_connect_back = 0" >>  $SETTINGS_FILE
-echo "xdebug.remote_autostart = on" >>  $SETTINGS_FILE
-echo "xdebug.idekey = PHPSTORM" >>  $SETTINGS_FILE
+
+# PLEASE NOTE THAT remote_connect_back needs to be disable for the remote_host to be used
+echo "xdebug.remote_autostart = 1
+xdebug.remote_connect_back = 0
+xdebug.remote_enable = 1
+xdebug.remote_host = $HOST_IP
+xdebug.idekey = PHPSTORM
+xdebug.remote_port = 9000" > $SETTINGS_FILE
 
